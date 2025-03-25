4c14f7e393da0609ba62263e9f68d84e53c8b213
xchen <xin.chen.ntu@gmail.com>
2025-03-25 12:07:20 -0400

    use dictionary to keep index

--- perfect_tricube_2.py
+++ perfect_tricube_2.py
@@ -6,12 +6,12 @@ input_num = 2000000
 start_time = time.time()
 
 # pre-compute all the cubes to prevent repetitiveness later, using a set instead of a list
-all_cubes = set()
+all_cubes = {}  # use a dictionary instead of a list
 n = 1
 while True:
     n_cube = n ** 3
     if n_cube < input_num:
-        all_cubes.add(n_cube)  # Use .add() to insert into a set
+        all_cubes[n_cube] = n  # Use .add() to insert into a set
         n += 1
     else:
         break
@@ -27,9 +27,10 @@ for i in range(1, max_cube_root):  # the root of the first cubelet ranges from 1
         j_cube = j ** 3
         for k in range(j, max_cube_root):  # the root of the third cubelet starts from j
             k_cube = k ** 3
+            ijk_cube_total = i_cube + j_cube + k_cube
             # Check if the sum of the cubes is in the set
-            if i_cube + j_cube + k_cube in all_cubes:
-                all_tricubes.append((i_cube, j_cube, k_cube, i, j, k))
+            if ijk_cube_total in all_cubes.keys():  # the dictionary keys is a set
+                all_tricubes.append((all_cubes[ijk_cube_total], ijk_cube_total, i, j, k))
 
 # print out all the tricubes
 print(len(all_tricubes), 'tricubes are found')
